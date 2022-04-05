cbc32b581345f21b1dc80cc32ef4d175127b9407
Maaha <awkcodergirl@gmail.com>
2022-04-05 22:55:35 +0500

    Completed RPS. Should commit more often

--- RockPaperScissors/javascript.js
+++ RockPaperScissors/javascript.js
@@ -1,67 +1,99 @@
-function computerPlay(){
-    const choice = Math.floor(Math.random() * 6);
-    
-    if (choice == 0 || choice == 1){
-        return "rock";
-    } else if (choice == 2 || choice == 3){
-        return "paper";
+let computerScore = 0, playerScore = 0, moves = 5;
+const pbuttons = document.querySelectorAll(".hbtn");
+
+pbuttons.forEach(button => {
+    button.addEventListener('click', function(e){
+        // console.log(e);
+        playRound(button.value);
+    })
+});
+
+function computerChoice(){
+    const compChoices = ["rock", "paper", "scissors"];
+    const result = compChoices[Math.floor(Math.random() * 3)];
+
+    const rock = document.querySelector(".crbtn");
+    const paper = document.querySelector(".cpbtn");
+    const scissors = document.querySelector(".csbtn");
+
+    if (result == "rock"){
+        rock.style.backgroundColor = "red";
+        rock.style.transition = '0.7s';
+        paper.style.backgroundColor = "aquamarine";
+        scissors.style.backgroundColor = "aquamarine"
+
+    } else if (result == "paper"){
+        paper.style.backgroundColor = "red";
+        paper.style.transition = '0.7s';
+        rock.style.backgroundColor = "aquamarine";
+        scissors.style.backgroundColor = "aquamarine"
+
+    } else if (result == "scissors"){
+        scissors.style.backgroundColor = "red";
+        scissors.style.transition = '0.7s';
+        paper.style.backgroundColor = "aquamarine";
+        rock.style.backgroundColor = "aquamarine"
     }
-
-    return "scissors";
+    return result;
 }
-
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-
-    if ((playerSelection == "rock" && computerSelection == "paper") ||
-    (playerSelection == "paper" && computerSelection == "scissors") ||
-    (playerSelection == "scissors" && computerSelection == "rock")){
-        return -1;
-        
     
-    } else if ((playerSelection == "paper" && computerSelection == "rock") ||
-    (playerSelection == "scissors" && computerSelection == "paper") ||
-    (playerSelection == "rock" && computerSelection == "scissors")){
-        return 1;
-        
-    
-    } else if ((playerSelection == "paper" && computerSelection == "paper") ||
-    (playerSelection == "rock" && computerSelection == "rock") || 
-    (playerSelection == "scissors" && computerSelection == "scissors")){
-        return 0;
-        
-    }
-}
 
-function game(){
-    let computerScore = 0, playerScore = 0;
-    let playerSelection, computerSelection;
-    for (let i = 0; i < 5; i++){
-        playerSelection = prompt("Enter your choice");
-        computerSelection = computerPlay();
-        console.log(playerSelection + " " + computerSelection);
-        let result = playRound(playerSelection, computerSelection);
-        if (result == 1){
-            console.log("You Win! " + playerSelection + " beats " + computerSelection);
-            playerScore++;
-        } else if (result == -1){
-            console.log("You Lose! " + computerSelection + " beats " + playerSelection);
-            computerScore++;
-        } else if (result == 0){
-            console.log("It's a draw! Try again :(");
+function playRound(playerSelection){
+        const computerSelection = computerChoice();
+        let result = "";
+        
+        console.log("p : ", playerSelection);
+        console.log("c : ", computerSelection);
+
+        if ((playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "rock")){
+            computerScore+=1;     
+            
+
+            if (computerScore == 5){
+                result = "Computer Wins!";
+                document.querySelector("#computerScore").innerHTML = computerScore;
+                disableButtons();
+            } else {
+                result = "You Lose! " + computerSelection + " beats " + playerSelection;
+                document.querySelector("#computerScore").innerHTML = computerScore;
+            }
+        
+        } else if ((playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "paper") ||
+        (playerSelection == "rock" && computerSelection == "scissors")){
+            playerScore+=1;  
+          
+           
+            
+            if (playerScore == 5){
+                result = "Player Wins!";
+                document.querySelector("#humanScore").innerHTML = playerScore;
+                
+                disableButtons();
+            } else {
+                result = "You Win! " + playerSelection + " beats " + computerSelection; 
+                document.querySelector("#humanScore").innerHTML = playerScore;
+            }
+        
+        } else if ((playerSelection == "paper" && computerSelection == "paper") ||
+        (playerSelection == "rock" && computerSelection == "rock") || 
+        (playerSelection == "scissors" && computerSelection == "scissors")){
+            result = "It's a draw! Try again :(";
         }
-    }
+        
+        document.querySelector("#result").innerHTML = result;
+        return;
 
-    console.log("Player Score: " + playerScore);
-    console.log("Computer Score:" + computerScore);
-
-    if (playerScore > computerScore){
-        console.log("You win!");
-    } else if (playerScore < computerScore){
-        console.log("Computer wins!")
-    } else {
-        console.log("Draw game!")
-    }
 }
 
-game();
+function playAgain(){
+
+}
+
+function disableButtons(){
+    pbuttons.forEach(button => {
+        button.disabled = true;
+    });
+}
