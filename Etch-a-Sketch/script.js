98936d1f003b59cae273da564ebc967f13bb92e1
Maaha <awkcodergirl@gmail.com>
2022-04-11 06:15:45 +0500

    Added a color picker to the Color Button. Made a grid too

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,14 +1,17 @@
-function makeGrid(){
-    const grid = document.querySelector(".innerSquare");
-    let div;
+function createDiv(){
+    const div = document.createElement("div");
+    // div.appendChild(document.createTextNode("here"));
+    div.setAttribute('style','border: 1px solid blue');
+    return div;
+}
 
-    for (let i = 1; i <= 16; i++){
-        for (let j = 1; j <= 16; j++){
-            div = document.createElement("div");
-            div.setAttribute('stlye','border: 1px solid black', 'padding: 5px');
-            grid.appendChild(div);
-        }
+const grid = document.querySelector(".screen");
+let docFrag = document.createDocumentFragment();
+
+for (let i = 1; i <= 16; i++){
+    for (let j = 1; j <= 16; j++){ 
+        docFrag.appendChild(createDiv());
     }
 }
 
-makeGrid();
+grid.appendChild(docFrag);
