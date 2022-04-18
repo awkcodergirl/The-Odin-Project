f2799601a082f3e5edb904e50da0d8b76223e722
Maaha <awkcodergirl@gmail.com>
2022-04-18 20:56:41 +0500

    The color picker function works. Have to fix the resolution slider

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -1,17 +1,50 @@
+// function that creates divs and appends them the the grid
 function createDiv(){
     const div = document.createElement("div");
-    // div.appendChild(document.createTextNode("here"));
-    div.setAttribute('style','border: 1px solid blue');
+    div.className = "gridSquare";
+    // div.setAttribute("style", "border: 1px solid blue");
     return div;
 }
 
-const grid = document.querySelector(".screen");
-let docFrag = document.createDocumentFragment();
-
-for (let i = 1; i <= 16; i++){
-    for (let j = 1; j <= 16; j++){ 
-        docFrag.appendChild(createDiv());
-    }
+// function that gets color from the color picker
+let color; 
+function colorPicker(){
+    color = document.querySelector(".colorPicker").value;
+    console.log(color);
 }
 
-grid.appendChild(docFrag);
+// function that records the slider value
+function changeResolution(){
+    let range = document.querySelector(".slider").value;
+    console.log(range);
+    // const screen = document.querySelector(".screen");
+    // for (let i = 1; i <= 16; i++){
+    //     for (let j = 1; j <= 16; j++){ 
+    //         screen.appendChild(createDiv());
+    //     }
+    // }
+}
+
+
+
+const screen = document.querySelector(".screen");
+
+let resolution = changeResolution();
+console.log("here");
+console.log("range: " + resolution);
+for (let i = 1; i <= 16; i++){
+    for (let j = 1; j <= 16; j++){ 
+        screen.appendChild(createDiv());
+    }
+}
+// screen.setAttribute("style","grid-template-columns: repeat(" + range + ", 1fr)");
+
+
+// hover over a div and change its color
+screen.addEventListener('mouseover', (e) => {
+    const square = e.target;
+    if (square.getAttribute('class') === "gridSquare"){
+        square.style.backgroundColor = color;
+    }    
+});
+
